2c6a9498b8b93d02f5452873604964cbbcc7c2c3
杨航源 <690094231@qq.com>
2017-09-14 17:10:37 +0800

    Geometric transformation1

---
+++ Graphic0.7/GraphicDoc.h
@@ -0,0 +1,64 @@
+// GraphicDoc.h : interface of the CGraphicDoc class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_GRAPHICDOC_H__B957CE6F_4E8C_4F51_818A_CF27BE0625F1__INCLUDED_)
+#define AFX_GRAPHICDOC_H__B957CE6F_4E8C_4F51_818A_CF27BE0625F1__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+
+class CGraphicDoc : public CDocument
+{
+protected: // create from serialization only
+	CGraphicDoc();
+	DECLARE_DYNCREATE(CGraphicDoc)
+
+// Attributes
+public:
+	//用于打开位图文件：
+	int numquad; //调色板数
+	int flagOpen;//标志是否打开bmp
+	BITMAPINFO* pbi;  //记录图像细节
+	BYTE* lpbuf;  //图像数据
+	RGBQUAD* quad;  //调色板
+	BITMAPFILEHEADER bf;//文件头
+	BITMAPINFOHEADER bi;  //信息头 
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CGraphicDoc)
+	public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CGraphicDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	//{{AFX_MSG(CGraphicDoc)
+	afx_msg void OnFileOpen();
+	afx_msg void OnFileSaveAs();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_GRAPHICDOC_H__B957CE6F_4E8C_4F51_818A_CF27BE0625F1__INCLUDED_)
---
+++ Graphic0.7/SettingDlg.cpp
@@ -0,0 +1,63 @@
+// SettingDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "Graphic.h"
+#include "SettingDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// SettingDlg dialog
+
+
+SettingDlg::SettingDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(SettingDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(SettingDlg)
+	m_LineWidth = 0;
+	m_LineStyle = 0;
+	m_nFILLMODEL = 1;
+	//}}AFX_DATA_INIT
+}
+
+
+void SettingDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(SettingDlg)
+	DDX_Text(pDX, IDC_LINEWIDTH, m_LineWidth);
+	DDX_Radio(pDX, IDC_LINEMODEL1, m_LineStyle);
+	DDX_Radio(pDX, IDC_FILL1, m_nFILLMODEL);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(SettingDlg, CDialog)
+	//{{AFX_MSG_MAP(SettingDlg)
+	ON_BN_CLICKED(IDC_LINEMODEL1, OnLineModel)
+	ON_BN_CLICKED(IDC_LINEMODEL2, OnLineModel)
+	ON_BN_CLICKED(IDC_LINEMODEL3, OnLineModel)
+	ON_BN_CLICKED(IDC_FILL1, OnFill)
+	ON_BN_CLICKED(IDC_FILL2, OnFill)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// SettingDlg message handlers
+
+void SettingDlg::OnLineModel() 
+{
+	// TODO: Add your control notification handler code here
+	UpdateData(true);
+}
+
+void SettingDlg::OnFill() 
+{
+	// TODO: Add your control notification handler code here
+	UpdateData(true);
+}
